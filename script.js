3db422b092601cf7e0eee11582a361524d1ced25
Brown-in-Michael <MICHAEL.BROWN@latoile.em-lyon.com>
2025-12-31 17:17:16 +0100

    Register

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function togglePassword() {
   const input = document.getElementById("password");
   input.type = input.type === "password" ? "text" : "password";
 }
+togglePassword();
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+isLeapYear();
+
+const inputDate = new Date(year, month - 1, day);
+const today = new Date();
+if (inputDate > today) {
+  // invalid
+}
+  const checkboxes = document.querySelectorAll('#gender input[type="checkbox"]');
+
+  checkboxes.forEach(box => {
+    box.addEventListener('change', () => {
+      checkboxes.forEach(other => {
+        if (other !== box) other.checked = false;
+      });
+    });
+  });
